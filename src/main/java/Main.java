0da78425d4e307a2b91ae0ea69d5c16e1b90909e
nabeelahmad <nabeelharis95@gmail.com>
2020-07-23 06:43:45 +0500

    Error test

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.print("Hello World");
+
+//        System.out.printf("Good work");
+
+//        System.out.flush();
+    }
+}
